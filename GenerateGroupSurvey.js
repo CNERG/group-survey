4db414d22d8c7a58f9f3cafb8d55df006e56bf16
Paul P.H. Wilson <paul.wilson@wisc.edu>
2018-04-26 06:49:02 -0500

    Add local copy of Javascript

---
+++ GenerateGroupSurvey.js
@@ -0,0 +1,43 @@
+function addMultipleChoice(form, questionData) {
+  
+    form.addMultipleChoiceItem()  
+        .setTitle(questionData.title)  
+        .setChoiceValues(questionData.choices)  
+        .setRequired(questionData.required); 
+    
+}
+
+function addTextItem(form, questionData) {
+    
+    form.addTextItem()  
+        .setTitle(questionData.title)  
+        .setRequired(questionData.required);  
+}
+
+//forceParam is a dummy parameter whose existence forces the UrlFetch to avoid caching
+function gennerateGroupSurvey(forceParam) {
+  
+  // Get the questions from some source in JSON format
+  var surveyQuestionsJSON = UrlFetchApp.fetch("https://raw.githubusercontent.com/CNERG/group-survey/master/questions.json");
+ 
+  var surveyQuestionsList = JSON.parse(surveyQuestionsJSON)
+  
+  var numQuestions = surveyQuestionsList.length
+  
+  var form
+  
+  if (numQuestions > 0) {
+      var formTitle = "Group Survey Form";  
+      var form = FormApp.create(formTitle)  
+          .setTitle(formTitle);  
+      
+      for (var i = 0; i < numQuestions; i++) {
+          Logger.log(surveyQuestionsList[i]);
+          if (surveyQuestionsList[i].type == "multipleChoice") {
+              addMultipleChoice(form, surveyQuestionsList[i])
+          } else if (surveyQuestionsList[i].type == "text") {
+              addTextItem(form, surveyQuestionsList[i])
+          }
+      }
+  }
+}
